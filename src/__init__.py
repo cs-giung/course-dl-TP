2e98eb86377821f12cf003042ee9b5696078dfbc
cs-giung <giung@korea.ac.kr>
2019-11-20 16:11:33 +0900

    .

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,4 @@ from .trainer import AverageMeter, ProgressMeter, accuracy, write_log
 from .vgg import VGG
 from .pgd import PGD_Linf, PGD_L2
 from .fgsm import FGSM
-from .atn import ATN
+from .atn import ATN, _atn_conv
