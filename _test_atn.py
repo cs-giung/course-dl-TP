e03b602bf719ba6b52c6c0735fdd6274b3618958
cs-giung <giung@korea.ac.kr>
2019-11-21 00:42:28 +0900

    /

--- _test_atn.py
+++ _test_atn.py
@@ -58,7 +58,7 @@ def main():
             if batch_idx == int(config['atn_sample'] * len(loader)):
                 break
             # lr = -0.0009 / config['atn_epoch'] * (epoch_idx - 1) + 0.001
-            lr = 0.0001
+            lr = 1e-5
             loss, l2_dist, li_dist = atn.train(images, labels, learning_rate=lr)
             losses.append(loss)
             l2_lst.append(l2_dist)
