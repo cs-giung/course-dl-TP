ff2e70adf50dea157c007e5ea24f51eb30d461f4
cs-giung <giung@korea.ac.kr>
2019-11-21 15:57:11 +0900

    .

--- src/atn.py
+++ src/atn.py
@@ -50,8 +50,8 @@ class ATN():
             _, ind_max = soft_labels[idx].max(0)
             _, ind_min = soft_labels[idx].min(0)
             soft_labels[idx][ind_max.item()] = alpha * soft_labels[idx][ind_min.item()]
-            norm = torch.norm(soft_labels[idx], p=2)
-            soft_labels[idx] = soft_labels[idx].div(norm.expand_as(soft_labels[idx]))
+        soft_labels_norm = torch.norm(soft_labels, dim=1, keepdim=True)
+        soft_labels = soft_labels / soft_labels_norm
         return soft_labels
 
     def train(self, images, labels, beta=0.99, learning_rate=0.001):
