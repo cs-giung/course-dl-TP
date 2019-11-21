ff93e025e8f721302139ccec8174ed33681e8039
cs-giung <giung@korea.ac.kr>
2019-11-21 22:02:56 +0900

    .

--- train_atn.py
+++ train_atn.py
@@ -224,7 +224,7 @@ def main():
 
                 a = sum(l2_lst) / len(l2_lst)
                 b = sum(linf_lst) / len(linf_lst)
-                print('[%5d/%5d] corr:%5d\tcorr_adv:%5d\tavg.l2:%.4f\tavg.linf:%.4f' % (batch_idx, len(loader), corr, corr_adv, a, b))
+                print('[%5d/%5d] corr:%5d\tcorr_adv:%5d\tavg.l2:%.4f\tavg.linf:%.4f' % (batch_idx, len(valid_loader), corr, corr_adv, a, b))
 
         # train & valid
         _ = train(train_loader, net, criterion, log_file, optimizer, epoch_idx, ATN=atn, config=config)
