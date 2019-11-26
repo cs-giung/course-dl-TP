f7a19ca724024f6a8e413321db5df3235bf2b679
cs-giung <giung@korea.ac.kr>
2019-11-26 22:13:54 +0900

    re-implement l2-pgd

--- src_attacks/pgd.py
+++ src_attacks/pgd.py
@@ -39,7 +39,7 @@ class PGD_Linf():
 
 class PGD_L2():
 
-    def __init__(self, model, epsilon=8*4/255, step_size=4/255, num_steps=10, random_start=True):
+    def __init__(self, model, epsilon=20*4/255, step_size=4/255, num_steps=10, random_start=True):
 
         self.model = model
         self.epsilon = epsilon
@@ -67,10 +67,12 @@ class PGD_L2():
             x = x + self.step_size * x.grad.sign()
             r = (x - images).view(x.size()[0], -1).detach()
             r_norm = torch.norm(r, dim=1, keepdim=True)
-            eps = torch.ones_like(r) * self.epsilon
-            eps_norm = torch.norm(eps, dim=1, keepdim=True)
-            mask = (r_norm < eps_norm).float()
-            r = torch.mul(r, mask) + torch.mul(eps_norm[0] * r / r_norm, 1 - mask)
+            # eps = torch.ones_like(r) * self.epsilon
+            # eps_norm = torch.norm(eps, dim=1, keepdim=True)
+            # mask = (r_norm < eps_norm).float()
+            # r = torch.mul(r, mask) + torch.mul(eps_norm[0] * r / r_norm, 1 - mask)
+            mask = (r_norm < self.epsilon).float()
+            r = torch.mul(r, mask) + torch.mul(self.epsilon * r / r_norm, 1 - mask)
             r = r.view(x.shape)
 
             x = torch.clamp(images + r, min=-2, max=2)
